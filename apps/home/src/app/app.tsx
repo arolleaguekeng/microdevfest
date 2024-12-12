e11883028d00d64a59b7fcb9e3eb86f4eef48058
arolleaguekeng <arolleaguekeng@gmail.com>
2024-12-12 20:39:37 +0100

    fix: update Speakers label styling with a red background color

--- apps/home/src/app/app.tsx
+++ apps/home/src/app/app.tsx
@@ -27,7 +27,7 @@ export function App() {
                     target="_blank"
                     className="cursor-pointer truncate transition-all bg-green-500 text-white px-6 py-2 rounded-lg border-green-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]"
                   >
-                    <span>Speakers</span>
+                    <span className="bg-red-600">Speakers</span>
                   </a>
 
                   <a
